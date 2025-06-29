a3bd3c54036246df68e26f5731dd6db9340971d3
Lep Sistemas <lepsistemas@gmail.com>
2025-06-29 18:47:07 -0400

    Add logs

--- launch.sh
+++ launch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /app
-PYTHONPATH=/app/src python3 /app/src/steamprotipsai/main.py
+LOGDIR="$HOME/.local/share/steamprotipsai"
+mkdir -p "$LOGDIR"
+LOGFILE="$LOGDIR/log.txt"
+
+PYTHONPATH=/app/src python3 /app/src/steamprotipsai/main.py >> "$LOGFILE" 2>&1
